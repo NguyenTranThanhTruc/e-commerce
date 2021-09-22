7536752d924eb1d643956f8a17a049df2ef7b9de
QuanLyThamQuan <55777063+NguyenTranThanhTruc@users.noreply.github.com>
2021-09-22 15:43:42 +0700

    add to cart

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,7 +11,8 @@ import {
 import { ShoppingCart } from "@material-ui/icons";
 import useStyle from "./style";
 import logo from "../../assets/commerce.png";
-const Navbar = () => {
+
+const Navbar = ({totalItems}) => {
   const classes = useStyle();
   return (
     <>
@@ -29,7 +30,7 @@ const Navbar = () => {
           <div className={classes.grow} />
           <div className={classes.button}>
             <IconButton aria-label="Show cart items" color="inherit">
-              <Badge badgeContent={2} color="secondary">
+              <Badge badgeContent={totalItems} color="secondary">
                 <ShoppingCart />
               </Badge>
             </IconButton>
